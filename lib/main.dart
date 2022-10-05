9f8e6fc503d576862a5a7596e7b78ce34586095f
wogks <wogks@wogksui-MacBookPro.local>
2022-10-05 16:38:52 +0900

    Feat: theme 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alyak/util/dory_themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,24 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        fontFamily: 'GmarketSansTTF'
-      ),
+      theme:DoryThemes.lightTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
