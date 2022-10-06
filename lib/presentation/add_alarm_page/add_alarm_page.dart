0e857681d0f15987dd2a9f7f267788037ac54523
wogks <wogks@wogksui-MacBookPro.local>
2022-10-07 03:01:12 +0900

    Feat: 시간 추가, 삭제 기능 추가

--- lib/presentation/add_alarm_page/add_alarm_page.dart
+++ lib/presentation/add_alarm_page/add_alarm_page.dart
@@ -4,10 +4,11 @@ import 'package:alyak/util/dory_constants.dart';
 import 'package:alyak/util/dory_widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../util/add_page_widget.dart';
 
-class AddAlarmPage extends StatelessWidget {
+class AddAlarmPage extends StatefulWidget {
   const AddAlarmPage({
     super.key,
     required this.medicineImage,
@@ -16,6 +17,17 @@ class AddAlarmPage extends StatelessWidget {
 
   final File? medicineImage;
   final String medicineName;
+
+  @override
+  State<AddAlarmPage> createState() => _AddAlarmPageState();
+}
+
+class _AddAlarmPageState extends State<AddAlarmPage> {
+  final _alarms = <String>{
+    '8:00',
+    '13:00',
+    '19:00',
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +46,7 @@ class AddAlarmPage extends StatelessWidget {
           ),
           const SizedBox(height: largeSpace),
           Expanded(
-            child: ListView(
-              children: const [
-                AlarmBox(),
-                AlarmBox(),
-                AlarmBox(),
-                AlarmBox(),
-                AddAlarmButton(),
-              ],
-            ),
+            child: ListView(children: alarmWidgets),
           ),
         ],
       ),
@@ -63,21 +67,58 @@ class AddAlarmPage extends StatelessWidget {
       ),
     );
   }
+
+  List<Widget> get alarmWidgets {
+    final children = <Widget>[];
+
+    children.addAll(
+      _alarms.map(
+        (alarmTime) => AlarmBox(
+          time: alarmTime,
+          onPressedMinus: () {
+            setState(() {
+              _alarms.remove(alarmTime);
+            });
+          },
+        ),
+      ),
+    );
+    children.add(AddAlarmButton(
+      onPressedPlus: () {
+        setState(() {
+          final now = DateTime.now();
+          //intl add dependency 해야함 시간 출력 포맷 변경
+          final nowTime = DateFormat('HH:mm').format(now);
+          _alarms.add(nowTime);
+        });
+      },
+    ));
+    return children;
+  }
 }
 
 class AlarmBox extends StatelessWidget {
   const AlarmBox({
     Key? key,
+    required this.time,
+    required this.onPressedMinus,
   }) : super(key: key);
+
+  final String time;
+  final VoidCallback onPressedMinus;
+
+  
 
   @override
   Widget build(BuildContext context) {
+    //time값을 datetime값으로 변환해야함
+    final initTime = DateFormat('HH:mm').parse(time);
     return Row(
       children: [
         Expanded(
           flex: 1,
           child: IconButton(
-            onPressed: () {},
+            onPressed: onPressedMinus,
             icon: const Icon(CupertinoIcons.minus_circle),
           ),
         ),
@@ -90,47 +131,71 @@ class AlarmBox extends StatelessWidget {
               showModalBottomSheet(
                 context: context,
                 builder: (context) {
-                  return BottomSheetBody(
-                    children: [
-                      SizedBox(
-                        height: 300,
-                        child: CupertinoDatePicker(
-                          onDateTimeChanged: (dateTime) {},
-                          mode: CupertinoDatePickerMode.time,
-                        ),
-                      ),
-                      const SizedBox(height: regularSpace,),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: SizedBox(
-                              height: submitButtonHeight,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(textStyle: Theme.of(context).textTheme.subtitle1),
-                                  onPressed: () {}, child: const Text('선택'),),
-                            ),
-                          ),
-                          const SizedBox(width: smallSpace),
-                          Expanded(
-                            child: SizedBox(
-                              height: submitButtonHeight,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(textStyle: Theme.of(context).textTheme.subtitle1,
-                                backgroundColor: Colors.white,
-                                foregroundColor: Colors.black
-                                 ),
-                                  onPressed: () {}, child: const Text('취소'),),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                  return TimePickerBottomSheet(
+                    initialDateTime: initTime,
                   );
                 },
               );
             },
-            child: const Text('18:00'),
+            child: Text(time),
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class TimePickerBottomSheet extends StatelessWidget {
+  const TimePickerBottomSheet({
+    Key? key, required this.initialDateTime,
+  }) : super(key: key);
+
+  final DateTime initialDateTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheetBody(
+      children: [
+        SizedBox(
+          height: 300,
+          child: CupertinoDatePicker(
+            onDateTimeChanged: (dateTime) {},
+            mode: CupertinoDatePickerMode.time,
+            //초기 시간 설정
+            initialDateTime: initialDateTime,
+          ),
+        ),
+        const SizedBox(
+          height: regularSpace,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: submitButtonHeight,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: Theme.of(context).textTheme.subtitle1),
+                  onPressed: () {},
+                  child: const Text('선택'),
+                ),
+              ),
+            ),
+            const SizedBox(width: smallSpace),
+            Expanded(
+              child: SizedBox(
+                height: submitButtonHeight,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: Theme.of(context).textTheme.subtitle1,
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black),
+                  onPressed: () {},
+                  child: const Text('취소'),
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
@@ -139,8 +204,10 @@ class AlarmBox extends StatelessWidget {
 
 class AddAlarmButton extends StatelessWidget {
   const AddAlarmButton({
-    Key? key,
+    Key? key, required this.onPressedPlus,
   }) : super(key: key);
+
+  final VoidCallback onPressedPlus;
 
   @override
   Widget build(BuildContext context) {
@@ -149,7 +216,7 @@ class AddAlarmButton extends StatelessWidget {
         textStyle: Theme.of(context).textTheme.subtitle2,
         padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6),
       ),
-      onPressed: () {},
+      onPressed: onPressedPlus,
       child: Row(
         children: const [
           Expanded(flex: 1, child: Icon(CupertinoIcons.plus_circle_fill)),
